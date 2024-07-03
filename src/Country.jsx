62d6cf21c4e8a6681a4a20b638074d24f9a940ea
DEEPAK GADWAL <deepak.gadwal008@@gmail.com>
2024-07-03 22:11:30 +0530

    add_console.error_2

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,34 +1,36 @@
 import React, { useEffect, useState } from "react";
-import styles from "./Country.module.css"
- const Country=()=>{
-const [countries,setCountries]= useState([])
-const API_ENDPOINT="https://xcountries-backend.azurewebsites.net/all"
+import styles from "./Country.module.css";
 
-useEffect(()=>{
-const fetchContry=async()=>{
-try{
-const res= await fetch(API_ENDPOINT)
-if (!res.ok) {
-  throw new Error("Network response was not ok");
-}
-const data= await res.json()
-setCountries(data)
-}catch(error){
-  console.error("Error Fetching data",error)
-}
-}
-fetchContry()
-},[])
-  return(
+const Country = () => {
+  const [countries, setCountries] = useState([]);
+  const API_ENDPOINT = "https://xcountries-backend.azurewebsites.net/all";
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      try {
+        const res = await fetch(API_ENDPOINT);
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await res.json();
+        setCountries(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchCountry();
+  }, []);
+
+  return (
     <div className={styles.mainCard}>
-{countries.map((country,idx)=>{
-  return <div key={idx} className={styles.cardWrapper}>
-  <img src={country.flag} alt={country.name} className={styles.cardImg} />
-  <h4>{country.name}</h4>
-  </div>
-})}
+      {countries.map((country, idx) => (
+        <div key={idx} className={styles.cardWrapper}>
+          <img src={country.flag} alt={country.name} className={styles.cardImg} />
+          <h4>{country.name}</h4>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-</div>
-  )
-}
 export default Country;
